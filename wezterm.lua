6c7e7df1a4fa2055cd78470994911113a0bb5a81
dawidwozny <92235733+dawidwozny@users.noreply.github.com>
2026-03-15 21:48:49 +0100

    navigate

--- wezterm.lua
+++ wezterm.lua
@@ -1,3 +1,4 @@
+local wezterm = require('wezterm')
 local Config = require('config')
 
 require('utils.backdrops')
@@ -11,6 +12,25 @@ require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
 require('events.tab-title').setup({ hide_active_tab_unseen = false, unseen_icon = 'numbered_box' })
 require('events.new-tab-button').setup()
 require('events.gui-startup').setup()
+
+-- Handle neovim smart-splits pane navigation via escape sequences
+wezterm.on('user-var-changed', function(window, pane, name, value)
+   if name == 'WEZTERM_NAVIGATE' then
+      local direction_map = {
+         Left = 'Left',
+         Right = 'Right',
+         Up = 'Up',
+         Down = 'Down',
+      }
+      local direction = direction_map[value]
+      if direction then
+         window:perform_action(
+            wezterm.action.ActivatePaneDirection(direction),
+            pane
+         )
+      end
+   end
+end)
 
 return Config:init()
    :append(require('config.appearance'))
